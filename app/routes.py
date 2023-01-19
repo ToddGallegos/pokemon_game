c87630302196dadb56827058a4019cbcf9e907c5
ToddGallegos <toroga@hotmail.com>
2023-01-19 03:26:01 -0800

    FINALLY!!!! stupid jinja

--- app/routes.py
+++ app/routes.py
@@ -11,24 +11,24 @@ def homepage():
 def pokemon():
     form = PokemonCatcherForm()
     if request.method == "POST":
-        if form.validate():
-            pokemon_name = form.pokemon_name.data
-            
-            def pokemon_info(p_name):
-                response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{p_name.lower()}')
-                
-                if response.ok:
-                    my_pokemon = {'pokemon_name': response.json()['forms'][0]['name'], 
-                                'base_hp': response.json()['stats'][0]['base_stat'], 
-                                'base_attack': response.json()['stats'][1]['base_stat'], 
-                                'base_defense': response.json()['stats'][2]['base_stat'], 
-                                'base_experience': response.json()['base_experience'], 
-                                'ability_name': response.json()['abilities'][0]['ability']['name'], 
-                                'front_shiny_sprite': response.json()['sprites']['front_shiny']}
-                    return my_pokemon
 
-            my_pokemon = pokemon_info(pokemon_name)
-            if my_pokemon:
-                return render_template('pokemon.html', form = form, my_pokemon = my_pokemon)
-            
-    return render_template('pokemon.html', form = form)
+        pokemon_name = form.pokemon_name.data
+        
+        def pokemon_info(p_name):
+            response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{p_name.lower()}')
+            if response.ok:
+                my_pokemon = {'pokemon_name': response.json()['forms'][0]['name'], 
+                            'base_hp': response.json()['stats'][0]['base_stat'], 
+                            'base_attack': response.json()['stats'][1]['base_stat'], 
+                            'base_defense': response.json()['stats'][2]['base_stat'], 
+                            'base_experience': response.json()['base_experience'], 
+                            'ability_name': response.json()['abilities'][0]['ability']['name'], 
+                            'front_shiny_sprite': response.json()['sprites']['front_shiny']}
+                return my_pokemon
+
+        the_pokemon = pokemon_info(pokemon_name)
+        print(the_pokemon)
+
+        return render_template('pokemon.html', form = form, the_pokemon = the_pokemon)
+    elif request.method == "GET":  
+        return render_template('pokemon.html', form = form)
